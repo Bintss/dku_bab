0331ecaa2537bd046e55ad5c845bacda3572b5a2
Taehonn Lee <32223673@dankook.ac.kr>
2025-12-02 15:49:34 +0900

    공지 admin/owner만 작성/수정/삭제 가능 구현

--- notices/models.py
+++ notices/models.py
@@ -6,7 +6,7 @@ class Notice(models.Model):
     """식당/서비스 공지사항"""
     title = models.CharField(max_length=200)
     content = models.TextField()
-    # 공지 작성자 (관리자) - 로그인/권한은 나중에 구현
+    # 공지 작성자 (admin/owner) - DRF permission + view에서 로그인 유저를 author로 설정
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
